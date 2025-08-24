506457ec11d620c43e026a7418318906b65d5396
Wonkanese <northkoreanwillywonka@gmail.com>
2025-08-23 19:17:25 -0600

    oops added wrong link

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://backend-4ig5niadd-h-websters-projects.vercel.app' : 'http://localhost:5000/api';
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://backend-six-sooty-74.vercel.app/' : 'http://localhost:5000/api';
 
 export const api = {
   async saveTeamSearch(teams) {
